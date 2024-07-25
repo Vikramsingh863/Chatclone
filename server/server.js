98b34eae760f5844acbdce18bca49b0507f9cc95
vikramsingh863 <vikramsinghrathore863@gmail.com>
2024-07-25 22:33:00 +0530

    cors setting

--- server/server.js
+++ server/server.js
@@ -9,7 +9,9 @@ dotenv.config();
 const PORT = 5500||process.env.PORT
 const MONGOID = process.env.MONGOID
 const MONGOPASS = process.env.MONGOPASS
-app.use(cors())
+app.use(cors({
+  origin: "https://chatclone-one.vercel.app"
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use('/', route)
